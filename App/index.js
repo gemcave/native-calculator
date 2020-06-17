0dd4dc4a4b10b431f76ce138fb6b699888b3e3f2
yerowyn <yerowyn@gmail.com>
2020-06-17 11:33:29 +0600

    add main App comp

---
+++ App/index.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import Button from './components/Button';
+import Row from './components/Row';
+
+import calculator, { initialState } from '../utils/calculator';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#202020',
+    justifyContent: 'flex-end',
+  },
+  value: {
+    color: '#fff',
+    fontSize: 40,
+    textAlign: 'right',
+    marginRight: 20,
+    marginBottom: 10,
+  },
+});
+
+export default function App() {
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    console.log(state);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state]);
+
+  const handleTap = (type, value) => {
+    setState((prevState) => calculator(type, value, prevState));
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView>
+        <Text style={styles.value}>
+          {parseFloat(state.currentValue).toLocaleString()}
+        </Text>
+        <Row>
+          <Button
+            text="C"
+            theme="secondary"
+            onPress={() => handleTap('clear')}
+          />
+          <Button
+            text="+/-"
+            theme="secondary"
+            onPress={() => handleTap('posneg')}
+          />
+          <Button
+            text="%"
+            theme="secondary"
+            onPress={() => handleTap('percentage')}
+          />
+          <Button
+            text="/"
+            theme="accent"
+            onPress={() => handleTap('operator', '/')}
+          />
+        </Row>
+
+        <Row>
+          <Button text="7" onPress={() => handleTap('number', 7)} />
+          <Button text="8" onPress={() => handleTap('number', 8)} />
+          <Button text="9" onPress={() => handleTap('number', 9)} />
+          <Button
+            text="x"
+            theme="accent"
+            onPress={() => handleTap('operator', '*')}
+          />
+        </Row>
+
+        <Row>
+          <Button text="4" onPress={() => handleTap('number', 4)} />
+          <Button text="5" onPress={() => handleTap('number', 5)} />
+          <Button text="6" onPress={() => handleTap('number', 6)} />
+          <Button
+            text="-"
+            theme="accent"
+            onPress={() => handleTap('operator', '-')}
+          />
+        </Row>
+
+        <Row>
+          <Button text="1" onPress={() => handleTap('number', 1)} />
+          <Button text="2" onPress={() => handleTap('number', 2)} />
+          <Button text="3" onPress={() => handleTap('number', 3)} />
+          <Button
+            text="+"
+            theme="accent"
+            onPress={() => handleTap('operator', '+')}
+          />
+        </Row>
+
+        <Row>
+          <Button
+            text="0"
+            size="double"
+            onPress={() => handleTap('number', 0)}
+          />
+          <Button text="." onPress={() => handleTap('number', '.')} />
+          <Button text="=" theme="accent" onPress={() => handleTap('equal')} />
+        </Row>
+      </SafeAreaView>
+    </View>
+  );
+}
